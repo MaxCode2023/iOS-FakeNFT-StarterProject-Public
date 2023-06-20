3cf37a2b22821bb44436a2fbbfe7c7182c34bf3f
MaxCode2023 <course.ios@artsofte.ru>
2023-06-20 23:21:24 +0500

    Частично реализована верстка экрана рейтинга пользователя

--- FakeNFT/Scenes /Statistic/Raiting/RatingViewController.swift
+++ FakeNFT/Scenes /Statistic/Raiting/RatingViewController.swift
@@ -7,11 +7,53 @@
 
 import UIKit
 
-final class RaitingViewController: UIViewController {
+final class RatingViewController: UIViewController {
  
+    private let sortImageView = UIImageView()
+    private let ratingTableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.addSubview(sortImageView)
+        view.addSubview(ratingTableView)
+        
+        ratingTableView.dataSource = self
+        ratingTableView.delegate = self
+        ratingTableView.register(UserRatingCell.self)
+        ratingTableView.separatorStyle = .none
+        
+        sortImageView.translatesAutoresizingMaskIntoConstraints = false
+        ratingTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        sortImageView.image = UIImage(named: "sortIcon")
+        
+        NSLayoutConstraint.activate([
+            sortImageView.heightAnchor.constraint(equalToConstant: 42),
+            sortImageView.widthAnchor.constraint(equalToConstant: 42),
+            sortImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 2),
+            sortImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -9),
+            
+            ratingTableView.topAnchor.constraint(equalTo: sortImageView.bottomAnchor, constant: 20),
+            ratingTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -8),
+            ratingTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            ratingTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
+}
+
+extension RatingViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UserRatingCell = tableView.dequeueReusableCell()
+        
+        cell.configure(index: 1, user: nil)
+        return cell
     }
     
 }
---
+++ FakeNFT/Scenes /Statistic/Raiting/UserRatingCell.swift
@@ -0,0 +1,80 @@
+//
+//  UserRatingCell.swift
+//  FakeNFT
+//
+//  Created by macOS on 20.06.2023.
+//
+
+import UIKit
+
+class UserRatingCell: UITableViewCell, ReuseIdentifying {
+        
+    private let mainStackView = UIStackView()
+    
+    private let indexLabel = UILabel()
+    private let backgroundCardView = UIView()
+    private let avatarImageView = UIImageView()
+    private let nameLabel = UILabel()
+    private let ratingScoreLabel = UILabel()
+    
+    func configure(index: Int, user: User?) {
+        selectionStyle = .none
+        contentView.addSubview(mainStackView)
+        
+        mainStackView.addArrangedSubview(indexLabel)
+        mainStackView.addArrangedSubview(backgroundCardView)
+        mainStackView.axis = .horizontal
+        mainStackView.spacing = 8
+        mainStackView.alignment = .center
+        
+        backgroundCardView.addSubview(avatarImageView)
+        backgroundCardView.addSubview(nameLabel)
+        backgroundCardView.addSubview(ratingScoreLabel)
+        
+        backgroundCardView.backgroundColor = .YPLightGrey
+        backgroundCardView.layer.cornerRadius = 12
+        
+        backgroundCardView.translatesAutoresizingMaskIntoConstraints = false
+        indexLabel.translatesAutoresizingMaskIntoConstraints = false
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        avatarImageView.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        ratingScoreLabel.translatesAutoresizingMaskIntoConstraints = false
+                
+        NSLayoutConstraint.activate([
+            mainStackView.heightAnchor.constraint(equalToConstant: 80),
+            mainStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            mainStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            mainStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            mainStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
+                        
+            backgroundCardView.heightAnchor.constraint(equalToConstant: 80),
+            indexLabel.widthAnchor.constraint(equalToConstant: 27),
+            
+            avatarImageView.centerYAnchor.constraint(equalTo: backgroundCardView.centerYAnchor),
+            nameLabel.centerYAnchor.constraint(equalTo: backgroundCardView.centerYAnchor),
+            ratingScoreLabel.centerYAnchor.constraint(equalTo: backgroundCardView.centerYAnchor),
+            
+            avatarImageView.heightAnchor.constraint(equalToConstant: 28),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 28),
+            
+            avatarImageView.leadingAnchor.constraint(equalTo: backgroundCardView.leadingAnchor, constant: 16),
+            nameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 8),
+            ratingScoreLabel.trailingAnchor.constraint(equalTo: backgroundCardView.trailingAnchor, constant: -16),
+            nameLabel.trailingAnchor.constraint(equalTo: ratingScoreLabel.leadingAnchor, constant: -26),
+        ])
+        
+        indexLabel.text = "12"
+        indexLabel.font = .caption1
+        indexLabel.textColor = .YPBlack
+        
+        nameLabel.text = "test Name"
+        nameLabel.font = .headline3
+        indexLabel.textColor = .YPBlack
+        
+        ratingScoreLabel.text = "121"
+        ratingScoreLabel.font = .headline3
+        indexLabel.textColor = .YPBlack
+    }
+    
+}
