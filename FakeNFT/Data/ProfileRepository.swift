1dfde03d125e229c7ce298dfee1bb2b304f114a5
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2023-06-23 16:26:03 +0300

    Сетевое взаимодействие для получения данных профиля

---
+++ FakeNFT/Data/ProfileRepository.swift
@@ -0,0 +1,10 @@
+//
+//  ProfileRepository.swift
+//  FakeNFT
+//
+//  Created by Суворов Дмитрий Владимирович on 23.06.2023.
+//
+
+final class ProfileRepository {
+    private let profileService = ProfileService.shared
+}
