8b0fa3df1fb8eadb47789acfe37c7cdb9891fe07
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2023-07-06 17:54:52 +0300

    Верстка кодом Tabbar

---
+++ FakeNFT/Scenes /Catalogue/CatalogueViewController.swift
@@ -0,0 +1,12 @@
+//
+//  CatalogueViewController.swift
+//  FakeNFT
+//
+//  Created by Суворов Дмитрий Владимирович on 06.07.2023.
+//
+
+import UIKit
+
+final class CatalogueViewController: UIViewController {
+
+}
